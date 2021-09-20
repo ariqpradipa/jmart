e1dfc3a840b70bff4221acee55637868da97a8ff
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-09-20 21:48:35 +0700

    revisi pt modul 2 - ngebenerin shipmentduration

--- AriqJmartFA/ShipmentDuration.java
+++ AriqJmartFA/ShipmentDuration.java
@@ -4,12 +4,12 @@ package AriqJmartFA;
 public class ShipmentDuration {
     
     public static ShipmentDuration INSTANT;
-    public static ShipmentDuration SAME_DAY ;
+    public static ShipmentDuration SAME_DAY;
     public static ShipmentDuration NEXT_DAY;
     public static ShipmentDuration REGULER;
     public static ShipmentDuration KARGO;
-    //private final int bit;
-    /*
+    private int bit;
+    
     private ShipmentDuration(int bit) {
 
         this.bit = bit;
@@ -18,17 +18,41 @@ public class ShipmentDuration {
 
     public ShipmentDuration(ShipmentDuration ... args) {
 
-        
-        
+        for(ShipmentDuration i: args) {
 
+            if(i == INSTANT) {
 
+                this.bit = this.bit | 1;
 
+            }
+            if(i == SAME_DAY) {
+
+                this.bit = this.bit | 2;
+
+            }
+            if(i == NEXT_DAY) {
+
+                this.bit = this.bit | 4;
+
+            }
+            if(i == REGULER) {
+
+                this.bit = this.bit | 8;
+
+            }
+            if(i == KARGO) {
+
+                this.bit = this.bit | 16;
+
+            }
+        }
     }
 
+    /*
     public boolean isDuration(ShipmentDuration reference) {
+        
+        
 
-
-
-    }
-    */
+    } */
+    
 }
