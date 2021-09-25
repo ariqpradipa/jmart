48ba718e00b00be690ceb65f0d550ced13c4a6f6
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-09-25 09:44:05 +0700

    finishin Recognizeable class

---
+++ AriqJmartFA/Rating.java
@@ -0,0 +1,13 @@
+package AriqJmartFA;
+
+
+/**
+ * Enumeration class Rating - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Rating
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ AriqJmartFA/Recognizeable.java
@@ -0,0 +1,31 @@
+package AriqJmartFA;
+
+public class Recognizeable {
+
+    public final int id;
+
+    protected Recognizeable(int id) {
+
+        this.id = id;
+
+    }
+
+    public boolean equals(Object object) {
+
+        if(object instanceof Recognizeable && ((Recognizeable)object).id == this.id) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+    }
+    
+    public boolean equals(Recognizeable recog) {
+
+        return (recog.id == this.id);
+
+    }
+}
