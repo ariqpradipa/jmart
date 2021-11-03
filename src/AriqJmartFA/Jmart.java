206b18c5a008d2c71df5a1186172dbeacf3aebe8
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-11-03 21:55:43 +0700

    TP - gson dan country

--- src/AriqJmartFA/Jmart.java
+++ src/AriqJmartFA/Jmart.java
@@ -1,5 +1,11 @@
 package AriqJmartFA;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 /**
  * praktikum oop
  *
@@ -8,9 +14,33 @@ package AriqJmartFA;
  */
 
 public class Jmart {
-    
+
+    class Country {
+
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+
+    }
+
     public static void main(String[] args) {
-        
+
+        String filepath = "src/db/city.json";
+        Gson gson = new Gson();
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
