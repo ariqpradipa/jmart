e857398f6d8c5c1c9f7194d3c6edcd5751e356af
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-12 13:06:19 +0700

    add javadoc to payment

--- src/main/java/com/AriqJmartFA/Payment.java
+++ src/main/java/com/AriqJmartFA/Payment.java
@@ -3,12 +3,23 @@ package com.AriqJmartFA;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Manage the payment of JMART transaction
+ */
 public class Payment extends Invoice {
 
     public ArrayList<Record> history = new ArrayList<Record>();
     public int productCount;
     public Shipment shipment;
 
+    /**
+     * Creating new payment object.
+     *
+     * @param buyerId the buyer ID.
+     * @param productId purchased product ID.
+     * @param productCount purchased product Count.
+     * @param shipment product shipment type.
+     */
     public Payment(int buyerId, int productId, int productCount, Shipment shipment) {
 
         super(buyerId, productId);
@@ -17,12 +28,21 @@ public class Payment extends Invoice {
 
     }
 
+    /**
+     * Get the total pay form purchased product.
+     *
+     * @param product product object.
+     * @return total price.
+     */
     public static double getTotalPay(Product product) {
 
         return product.price;
 
     }
 
+    /**
+     * Record history list of payment.
+     */
     public static class Record {
 
         public final Date date = new Date();
