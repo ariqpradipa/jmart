b7c4a2439d0708af490d6a9457481e9495e9df99
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-12 13:03:30 +0700

    add javadoc to pair

--- src/main/java/com/AriqJmartFA/Pair.java
+++ src/main/java/com/AriqJmartFA/Pair.java
@@ -5,10 +5,18 @@ public class Pair<T, U> {
     public T first;
     public U second;
 
+    /**
+     * to call pair
+     */
     public Pair() {
 
     }
 
+    /**
+     *
+     * @param first T value first
+     * @param second T value second
+     */
     public Pair(T first, U second) {
 
         this.first = first;
