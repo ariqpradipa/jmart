37358baf75ebb4419fb550ee18564fb04c3f713b
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-12 13:11:11 +0700

    add javadoc to PhoneTopUp

--- src/main/java/com/AriqJmartFA/PhoneTopUp.java
+++ src/main/java/com/AriqJmartFA/PhoneTopUp.java
@@ -1,15 +1,32 @@
 package com.AriqJmartFA;
 
+/**
+ * For topup by phone number
+ */
 public class PhoneTopUp extends Invoice {
 
     public String phoneNumber;
     public Status status;
 
+    /**
+     * topup object
+     * @param buyerId the buyer ID
+     * @param productId product ID
+     * @param phoneNumber user phone number
+     */
     public PhoneTopUp(int buyerId, int productId, String phoneNumber) {
+
         super(buyerId, productId);
         this.phoneNumber = phoneNumber;
+
     }
 
+    /**
+     * Get total pay from topup
+     *
+     * @param product the product
+     * @return return get total pay
+     */
     public double getTotalPay(Product product) {
 
         return 0;
