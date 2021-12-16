884d3a87637b9431ffb50c4777471b7056f49ee9
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2021-12-16 19:41:01 +0700

    predicate javadoc

--- src/main/java/com/AriqJmartFA/Predicate.java
+++ src/main/java/com/AriqJmartFA/Predicate.java
@@ -2,6 +2,11 @@ package com.AriqJmartFA;
 
 public interface Predicate<T> {
 
+    /**
+     *
+     * @param arg data perdicate.
+     * @return args data.
+     */
     boolean predicate(T arg);
 
 }
